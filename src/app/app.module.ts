9c34662a7a8b587cb5703690fcdfc0e3310ebda9
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2018-12-01 12:27:01 -0500

    Add : Router de la aplicaion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AsesoresComponent } from './asesores/asesores.component';
+import { RouterModule, Routes } from '@angular/router';
+
+// Rutas del proyecto
+const appRoutes: Routes = [
+  { path: 'asesores', component: AsesoresComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, AsesoresComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

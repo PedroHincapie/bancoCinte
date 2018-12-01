768a548b1df2c24a3b057444f84504da31cab06d
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2018-12-01 15:14:16 -0500

    Crear componente de creacion de asesores

---
+++ src/app/nuevo-asesor/nuevo-asesor.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NuevoAsesorComponent } from './nuevo-asesor.component';
+
+describe('NuevoAsesorComponent', () => {
+  let component: NuevoAsesorComponent;
+  let fixture: ComponentFixture<NuevoAsesorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NuevoAsesorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NuevoAsesorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/nuevo-asesor/nuevo-asesor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-asesor',
+  templateUrl: './nuevo-asesor.component.html',
+  styleUrls: ['./nuevo-asesor.component.css']
+})
+export class NuevoAsesorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

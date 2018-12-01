4f8727cdef65a2ddd3a280b685ef0e5a27241f50
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2018-12-01 12:40:36 -0500

    Update : adicion del componente cliente al router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AsesoresComponent } from './asesores/asesores.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes.component';
 
 // Rutas del proyecto
 const appRoutes: Routes = [
-  { path: 'asesores', component: AsesoresComponent }
+  { path: 'asesores', component: AsesoresComponent},
+  {path: 'clientes', component: ClientesComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, AsesoresComponent],
+  declarations: [AppComponent, AsesoresComponent, ClientesComponent],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
